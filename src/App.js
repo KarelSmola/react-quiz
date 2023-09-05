924441aa9b98b1edc69542e5a73ac522a2f9e533
Karel Smola <karelsmola21@gmail.com>
2023-09-05 22:07:04 +0200

    New project created. Data fetching added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:9000/questions`);
+      const data = await response.json();
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return <div></div>;
+};
 
 export default App;
